245646d1a0150424b75e3d55e98b2f88e479a680
Sabyasachi Bhoi <sabyabhoi@gmail.com>
2020-06-20 16:16:26 +0530

    Worked on the post details

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -7,5 +7,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
   path('', views.PostList.as_view(), name='home'),
-#  path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'), 
+  path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
