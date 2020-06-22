d2c418bcd0ffbe71d0049e4e6cfc660077d9f335
Sabyasachi Bhoi <sabyabhoi@gmail.com>
2020-06-22 17:17:15 +0530

    Implementing Commenting System

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,5 +1,7 @@
 from django.views import generic
 from .models import Post
+from .forms import CommentForm
+from django.shortcuts import render, get_object_or_404
 
 class PostList(generic.ListView): 
   queryset = Post.objects.filter(status=1).order_by('-created_on') 
@@ -8,3 +10,19 @@ class PostList(generic.ListView):
 class PostDetail(generic.DetailView):
   model = Post
   template_name = 'post_detail.html'
+
+def post_detail(request, slug):
+  template_name = 'post_detail.html'
+  post = get_object_or_404(Post, slug=slug)
+  comments = post.comments.filter(active=True)
+  new_comment = None
+  if request.method == 'POST':
+    comment_form = CommentForm(data=request.POST)
+    if comment_form.is_valid():
+      new_comment = comment_form.save(commit=False)
+      new_comment.post = post
+      new_comment.save()
+  else:
+    comment_form = CommentForm()
+
+  return render(request, template_name, {'post': post, 'comments': comments, 'new_comment': new_comment, 'comment_form': comment_form})
